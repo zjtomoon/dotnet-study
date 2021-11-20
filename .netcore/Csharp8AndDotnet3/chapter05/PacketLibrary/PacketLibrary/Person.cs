4d86dac6943fad4327bee1b236ef1ec57735c04e
zjtomoon <zjtomoon@outlook.com>
2021-11-20 20:42:09 +0800

    default commit

--- .netcore/Csharp8AndDotnet3/chapter05/PacketLibrary/PacketLibrary/Person.cs
+++ .netcore/Csharp8AndDotnet3/chapter05/PacketLibrary/PacketLibrary/Person.cs
@@ -38,11 +38,58 @@ namespace Packet.Shared
         {
             return $"{Name} was born on {HomePlanet}.";
         }
+
+        public (string, int) GetFruit()
+        {
+            return ("Apple", 5);
+        }
+
+        public (string Name,int Number) GetNamedFruit()
+        {
+            return ( Name: "Apples", Number: 5);
+        }
+
+        public string SayHello()
+        {
+            return $"{Name} says 'Hello!'";
+        }
+
+        public string SayHelloTo(string name)
+        {
+            return $"{Name} says 'Hello {name} !'";
+        }
+
+        //创建一个带三个参数的方法
+        public string OptionalParameters(      
+            string command = "Run!",
+            double number = 0.0,
+            bool active = true)
+        {
+            return string.Format(format:"command is {0},number is {1},active is {2}",
+                arg0: command,arg1: number,arg2: active);
+        }
     }
     public class BankAccount
     {
         public string AccountName;
         public decimal Balance;
         public static decimal InterestRate;
+    }
+
+    public class TextAndNumber
+    {
+        public string Text;
+        public int Number;
+    }
+    public class Processor
+    {
+        public TextAndNumber GetAndNumber()
+        {
+            return new TextAndNumber
+            {
+                Text = "What's the meaning of life?",
+                Number = 42
+            };
+        }
     }
 }
--- .netcore/Csharp8AndDotnet3/chapter05/PeopleApp/PeopleApp/Program.cs
+++ .netcore/Csharp8AndDotnet3/chapter05/PeopleApp/PeopleApp/Program.cs
@@ -83,6 +83,24 @@ namespace PeopleApp
             //ReadLine();
             bob.WriteToConsole();
             WriteLine(bob.GetOrigin());
+
+            (string,int) fruit = bob.GetFruit();
+            WriteLine($"{fruit.Item1},{ fruit.Item2} there are.");
+
+            var fruitNamed = bob.GetNamedFruit();
+            WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}");
+
+            var thing1 = ("Neville", 4);
+            WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
+
+            (string fruitName, int fruitNumber) = bob.GetFruit();
+            WriteLine($"Deconstructed: {fruitName},{fruitNumber}");
+
+            WriteLine(bob.SayHello());
+            WriteLine(bob.SayHelloTo("Emily"));
+
+
+            WriteLine(bob.OptionalParameters());
         }
     }
 }
