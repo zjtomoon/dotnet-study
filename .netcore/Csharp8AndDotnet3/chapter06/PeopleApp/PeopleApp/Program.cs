e2aa90e2ff7360fb41725cee07613d5062aa74ee
zjtomoon <zjtomoon@outlook.com>
2021-11-21 22:05:08 +0800

    default commit

--- .netcore/Csharp8AndDotnet3/chapter06/PeopleApp/PeopleApp/Program.cs
+++ .netcore/Csharp8AndDotnet3/chapter06/PeopleApp/PeopleApp/Program.cs
@@ -251,6 +251,11 @@ namespace PeopleApp
             WriteLine("{0} squared is {1}",
                 arg0:number2,
                 arg1:Squarer.Square(number2));
+
+            var dv1 = new DisplacementVector(3, 5);
+            var dv2 = new DisplacementVector(-2, 7);
+            var dv3 = dv1 + dv2;
+            WriteLine($"({dv1.X},{dv1.Y}) + ({dv2.X} , {dv2.Y}) = ({dv3.X}, {dv3.Y})");
         }
 
         private static void Harry_Shout(object sender, EventArgs e)
