3d93a15e882eab63e399074da85a172249946f0b
zjtomoon <zjtomoon@outlook.com>
2021-11-23 17:21:33 +0800

    default commit

---
+++ .netcore/Csharp8AndDotnet3/chapter08/WorkingWithNumbers/WorkingWithNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+using static System.Console;
+namespace WorkingWithNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var largest = ulong.MaxValue;
+            WriteLine($"{largest,40:N0}");
+            var atomsInTheUniverse = BigInteger.Parse("123456789012345678901234567890");
+            WriteLine($"{atomsInTheUniverse,40:N0}");
+        }
+    }
+}
