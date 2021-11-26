3c2e2cb03d59f87661c7cbee1960d29f32182594
zjtomoon <zjtomoon@outlook.com>
2021-11-26 16:00:41 +0800

    default commit

---
+++ Csharp8AndDotnet3/chapter08/WorkingWithNetworkResources/WorkingWithNetworkResources/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using static System.Console;
+using System.Net.NetworkInformation;
+
+namespace WorkingWithNetworkResources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter a valid web address: ");
+            string url = ReadLine();
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                url = "https://world.episerver.com/cms/?q=pagetype";
+            }
+
+            var uri = new Uri(url);
+            WriteLine($"URL: {url}");
+            WriteLine($"Scheme: {uri.Scheme}");
+            WriteLine($"Port: {uri.Port}");
+            WriteLine($"Host : {uri.Host}");
+            WriteLine($"Path: {uri.AbsolutePath}");
+            WriteLine($"Query: {uri.Query}");
+
+            //得到所输入网站的IP地址
+            IPHostEntry entry = Dns.GetHostEntry(uri.Host);
+            WriteLine($"{entry.HostName} has the following IP address");
+            foreach (IPAddress address in entry.AddressList)
+            {
+                WriteLine($" {address}");
+            }
+            
+            //ping服务器
+            try
+            {
+                var ping = new Ping();
+                WriteLine("Pinging server. Please wait...");
+                PingReply reply = ping.Send(uri.Host);
+                WriteLine($"{uri.Host} was pinged and replied: {reply.Status}.");
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    WriteLine("Reply from {0} took {1:N0}ms",reply.Address,reply.RoundtripTime);
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{ex.GetType().ToString()} says {ex.Message}");
+            }
+        }
+    }
+}
--- Csharp8AndDotnet3/chapter08/WorkingWithRegularExpression/WorkingWithRegularExpression/Program.cs
+++ Csharp8AndDotnet3/chapter08/WorkingWithRegularExpression/WorkingWithRegularExpression/Program.cs
@@ -11,8 +11,14 @@ namespace WorkingWithRegularExpression
         {
             WriteLine("Enter your age: ");
             string input = ReadLine();
-
-            var ageCheck = new Regex(@"\d");
+            
+            //var ageCheck = new Regex(@"\d");
+            //var ageCheck = new Regex(@"^\d$");//拒绝除个位数以外的任何数
+            var ageCheck = new Regex(@"^\d+$");
+            /*
+             * @字符关闭了字符串中使用转义字符的功能。
+             * 在使用@禁用转义字符后，就可以用正则表达式解释它们。\d表示数字
+             */
             if (ageCheck.IsMatch(input))
             {
                 WriteLine("Thank you!");
@@ -20,6 +26,21 @@ namespace WorkingWithRegularExpression
             else
             {
                 WriteLine($"This is not a valid age: {input}");
+            }
+
+            string films = "\"Monsters, Inc.\",\",\"I,Tonya\",\"Lock,Stock and Two Smoking Barrels\"";
+            string[] filmsDumb = films.Split(',');
+            foreach (string film in filmsDumb)
+            {
+                WriteLine(film);
+            }
+            //添加语句以定义要分割的正则表达式
+            var csv = new Regex("(?:^|,)(?=[^\"]|(\")?)\"?((?(1)[^\"]*|[^,\"]*))\"?(?=,|$)");
+            MatchCollection filmSmart = csv.Matches(films);
+            WriteLine("Smart attempt at splitting:");
+            foreach (Match film in filmSmart)
+            {
+                WriteLine(film.Groups[2].Value);
             }
         }
     }
