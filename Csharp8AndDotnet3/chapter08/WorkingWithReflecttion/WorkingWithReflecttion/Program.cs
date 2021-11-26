b143864ddaa8e42a0d78e33f2b46740e0e1bc832
zjtomoon <zjtomoon@outlook.com>
2021-11-26 21:43:48 +0800

    default commit

---
+++ Csharp8AndDotnet3/chapter08/WorkingWithReflecttion/WorkingWithReflecttion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+using System.Reflection;
+
+namespace WorkingWithReflecttion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Assembly metadata:");
+            Assembly assembly = Assembly.GetEntryAssembly();
+            WriteLine($" Full name: {assembly.FullName}");
+            WriteLine($" Location: {assembly.Location}");
+
+            var attributes = assembly.GetCustomAttributes();
+            WriteLine($" Attributes:");
+            foreach (Attribute a in attributes)
+            {
+                WriteLine($" {a.GetType()}");
+            }
+            
+        }
+    }
+}
