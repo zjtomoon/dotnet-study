ff43c17234cd02c6e65507c9844298d48c9425ff
zjtomoon <zjtomoon@outlook.com>
2021-11-28 17:03:57 +0800

    default commit

---
+++ Csharp8AndDotnet3/chapter10/CryptographyLib/CryptographyLib/Protector.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Xml.Linq;
+using static System.Convert;
+
+
+namespace Packt.Shared
+{
+    public static class Protector
+    {
+        //salt size must be at least 8 bytes
+        private static readonly byte[] salt = Encoding.Unicode.GetBytes("7BANANAS");
+
+        //至少1000
+        private static readonly int iterations = 2000;
+
+        public static string Encrypt(string plainText, string password)
+        {
+            byte[] encryptedBytes;
+            byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms,aes.CreateEncryptor(),CryptoStreamMode.Write))
+                {
+                    cs.Write(plainBytes,0,plainBytes.Length);
+                }
+                encryptedBytes = ms.ToArray();
+            }
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        public static string Decrypt(string cryptoText, string password)
+        {
+            byte[] plainBytes;
+            byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms,aes.CreateDecryptor(),CryptoStreamMode.Write))
+                {
+                    cs.Write(cryptoBytes,0,cryptoBytes.Length);
+                }
+
+                plainBytes = ms.ToArray();
+            }
+
+            return Encoding.Unicode.GetString(plainBytes);
+        }
+
+        private static Dictionary<string, User> Users = new Dictionary<string, User>();
+        
+        public static User Register(string username,string password)
+        {
+            var rng = RandomNumberGenerator.Create();
+            var saltBytes = new byte[16];
+            rng.GetBytes(saltBytes);
+            var saltText = Convert.ToBase64String(saltBytes);
+            var saltedhashedPassword = SaltAndHashPassword(password, saltText);
+            var user = new User
+            {
+                Name = username, Salt = saltText,
+                SaltedHashedPassword = saltedhashedPassword
+            };
+            Users.Add(user.Name,user);
+            return user;
+
+        }
+
+        public static bool CheckPassword(string username, string password)
+        {
+            if (!Users.ContainsKey(username))
+            {
+                return false;
+            }
+
+            var user = Users[username];
+
+            var saltedhashedPassword = SaltAndHashPassword(password, user.Salt);
+            return (saltedhashedPassword == user.SaltedHashedPassword);
+        }
+
+        public static string SaltAndHashPassword(string password, string salt)
+        {
+            var sha = SHA256.Create();
+            var saltedPassword = password + salt;
+            return Convert.ToBase64String(
+                sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+        }
+    }
+}
---
+++ Csharp8AndDotnet3/chapter10/EncryptionApp/EncryptionApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Security.Cryptography;
+using Packt.Shared;
+using static System.Console;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a message that you want to encrypt: ");
+            string message = ReadLine();
+            Write("Enter a password: ");
+            string password = ReadLine();
+
+            string cryptoText = Protector.Encrypt(message, password);
+            WriteLine($"Encrypted text: {cryptoText}");
+            Write("Enter the password: ");
+            string password2 = ReadLine();
+
+            try
+            {
+                string clearText = Protector.Decrypt(cryptoText, password2);
+                WriteLine($"Decrypted text : {clearText}");
+            }
+            catch (CryptographicException ex)
+            {
+                WriteLine("{0}\nMore details: {1}",
+                    arg0: "You entered the wrong password",
+                    arg1: ex.Message);
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Non-cryptographic exception: {0} , {1}",
+                    arg0: ex.GetType().Name,
+                    arg1: ex.Message);
+            }
+            
+            WriteLine("Registering Alice with Pa$$0rd.");
+            var alice = Protector.Register("Alice", "Pa$$w0rd");
+            WriteLine($"Name : {alice.Name}");
+            WriteLine($"Salt: {alice.Salt}");
+            WriteLine("Password (salted and hashed): {0}",arg0: alice.SaltedHashedPassword);
+            WriteLine();
+            
+            Write("Enter a new user to register:");
+            string username = ReadLine();
+            Write($"Enter a password for {username}:");
+            string password3 = ReadLine();
+            var user = Protector.Register(username, password3);
+            WriteLine($"Name : {user.Name}");
+            WriteLine($"Salt: {user.Salt}");
+            WriteLine("Password (salted and hasherd): {0}",
+                arg0: user.SaltedHashedPassword);
+            WriteLine();
+
+            bool correctPassword = false;
+            while (!correctPassword)
+            {
+                Write("Enter a username to login: ");
+                string loginUsername = ReadLine();
+                Write("Enter a password to login: ");
+                string loginPassword = ReadLine();
+                correctPassword = Protector.CheckPassword(loginUsername, loginPassword);
+
+                if (correctPassword)
+                {
+                    WriteLine($"Correct! {loginUsername} has been logged in.");
+                }
+                else
+                {
+                    WriteLine("Invalid username or password.Try again.");
+                }
+            }
+        }
+    }
+}
