b4968186a5687b4cd6e853f7d5cb918c7705fb9c
zjtomoon <zjtomoon@outlook.com>
2021-11-29 14:11:00 +0800

    default commit

---
+++ Csharp8AndDotnet3/chapter10/SecureAPP/CryptographyLib/CryptographyLib/Protector.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Xml.Linq;
+using static System.Convert;
+
+
+namespace Packt.Shared
+{
+    public static class Protector
+    {
+        //salt size must be at least 8 bytes
+        private static readonly byte[] salt = Encoding.Unicode.GetBytes("7BANANAS");
+
+        //至少1000
+        private static readonly int iterations = 2000;
+
+        public static string Encrypt(string plainText, string password)
+        {
+            byte[] encryptedBytes;
+            byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(plainBytes, 0, plainBytes.Length);
+                }
+
+                encryptedBytes = ms.ToArray();
+            }
+
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        public static string Decrypt(string cryptoText, string password)
+        {
+            byte[] plainBytes;
+            byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(cryptoBytes, 0, cryptoBytes.Length);
+                }
+
+                plainBytes = ms.ToArray();
+            }
+
+            return Encoding.Unicode.GetString(plainBytes);
+        }
+
+        private static Dictionary<string, User> Users = new Dictionary<string, User>();
+
+        public static User Register(string username, string password,string[] roles = null)
+        {
+            var rng = RandomNumberGenerator.Create();
+            var saltBytes = new byte[16];
+            rng.GetBytes(saltBytes);
+            var saltText = Convert.ToBase64String(saltBytes);
+            var saltedhashedPassword = SaltAndHashPassword(password, saltText);
+            var user = new User
+            {
+                Name = username, Salt = saltText,
+                SaltedHashedPassword = saltedhashedPassword,
+                Roles = roles
+            };
+            Users.Add(user.Name, user);
+            return user;
+        }
+
+        public static bool CheckPassword(string username, string password)
+        {
+            if (!Users.ContainsKey(username))
+            {
+                return false;
+            }
+
+            var user = Users[username];
+
+            var saltedhashedPassword = SaltAndHashPassword(password, user.Salt);
+            return (saltedhashedPassword == user.SaltedHashedPassword);
+        }
+
+        public static string SaltAndHashPassword(string password, string salt)
+        {
+            var sha = SHA256.Create();
+            var saltedPassword = password + salt;
+            return Convert.ToBase64String(
+                sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+        }
+
+        public static string PublicKey;
+
+        //将RSA实例转换为XML
+        public static string ToXmlStringExt(this RSA rsa, bool includePrivateParameters)
+        {
+            var p = rsa.ExportParameters(includePrivateParameters);
+            XElement xml;
+            if (includePrivateParameters)
+            {
+                xml = new XElement("RSAKeyValue",
+                    new XElement("Modulus", ToBase64String(p.Modulus)),
+                    new XElement("Exponent", ToBase64String(p.Exponent)),
+                    new XElement("P", ToBase64String(p.P)),
+                    new XElement("Q", ToBase64String(p.Q)),
+                    new XElement("DP", ToBase64String(p.DP)),
+                    new XElement("DQ", ToBase64String(p.DQ)),
+                    new XElement("InverseQ", ToBase64String(p.InverseQ)));
+            }
+            else
+            {
+                xml = new XElement("RSAKeyValue", new XElement("Modulus", ToBase64String(p.Modulus),
+                    new XElement("Exponent", ToBase64String(p.Exponent))));
+            }
+
+            return xml?.ToString(); //?.表示Null检查运算符
+        }
+
+        //将XML实例转换为RSA实例
+        public static void FromXmlStringExt(this RSA rsa, string parametersAsXml)
+        {
+            var xml = XDocument.Parse(parametersAsXml);
+            var root = xml.Element("RSAKeyValue");
+            var p = new RSAParameters
+            {
+                Modulus = FromBase64String(root.Element("Modulus").Value),
+                Exponent = FromBase64String(root.Element("Exponent").Value)
+            };
+            if (root.Element("p") != null)
+            {
+                p.P = FromBase64String(root.Element("P").Value);
+                p.Q = FromBase64String(root.Element("Q").Value);
+                p.DP = FromBase64String(root.Element("DP").Value);
+                p.DQ = FromBase64String(root.Element("DQ").Value);
+                p.InverseQ = FromBase64String(root.Element("InverseQ").Value);
+            }
+            rsa.ImportParameters(p);
+        }
+
+        //生成签名
+        public static string GenerateSignature(string data)
+        {
+            byte[] dataBytes = Encoding.Unicode.GetBytes(data);
+            var sha = SHA256.Create();
+            var hashedData = sha.ComputeHash(dataBytes);
+            var rsa = RSA.Create();
+            PublicKey = rsa.ToXmlStringExt(false);
+            return ToBase64String(rsa.SignHash(hashedData, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1));
+        }
+        
+        //验证签名
+        public static bool ValidateSignature(string data, string signature)
+        {
+            byte[] dataBytes = Encoding.Unicode.GetBytes(data);
+            var sha = SHA256.Create();
+            var hashedData = sha.ComputeHash(dataBytes);
+            byte[] signatureBytes = FromBase64String(signature);
+            var rsa = RSA.Create();
+            rsa.FromXmlStringExt(PublicKey);
+
+            return rsa.VerifyHash(hashedData, signatureBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        }
+        
+        //为密码生成随机数
+        public static byte[] GetRandomKeyOrIV(int size)
+        {
+            var r = RandomNumberGenerator.Create();
+            var data = new byte[size];
+            r.GetNonZeroBytes(data);
+            return data;
+        }
+        
+        //定义LogIn方法，从而登录用户并使用通用标识和主体将他们分配给当前线程
+        public static void LogIn(string username, string password)
+        {
+            if (CheckPassword(username, password))
+            {
+                var identity = new GenericIdentity(username, "PacktAuth");
+                var principal = new GenericPrincipal(identity, Users[username].Roles);
+                System.Threading.Thread.CurrentPrincipal = principal;
+            }
+        }
+    }
+}
