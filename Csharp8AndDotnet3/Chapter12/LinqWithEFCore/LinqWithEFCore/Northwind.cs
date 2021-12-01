a66c1462b965e20e1bff1f37b73b33b6ef87f451
zjtomoon <zjtomoon@outlook.com>
2021-12-01 09:35:45 +0800

    default commit

---
+++ Csharp8AndDotnet3/Chapter12/LinqWithEFCore/LinqWithEFCore/Northwind.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Packt.Shared
+{
+    public class Northwind : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = System.IO.Path.Combine(
+                System.Environment.CurrentDirectory, "Northwind.db");
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+    }
+}
--- Csharp8AndDotnet3/Chapter12/LinqWithEFCore/LinqWithEFCore/Program.cs
+++ Csharp8AndDotnet3/Chapter12/LinqWithEFCore/LinqWithEFCore/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using static System.Console;
 
 namespace LinqWithEFCore
 {
@@ -6,6 +10,25 @@ namespace LinqWithEFCore
     {
         static void Main(string[] args)
         {
+            FilterAndSort();
+        }
+
+        static void FilterAndSort()
+        {
+            using (var db = new Northwind())
+            {
+                var query = db.Products
+                    .Where(product => product.UnitPrice < 10M )
+                    // IQueryable<Product>
+                    .OrderByDescending(product => product.UnitPrice);
+                WriteLine("Products that cost less than $10");
+                foreach (var item in query)
+                {
+                    WriteLine("{0} : {1} cost {2:$#,##0.00}",
+                        item.ProductID,item.ProductName,item.UnitPrice);
+                }
+                WriteLine();
+            }
         }
     }
 }
