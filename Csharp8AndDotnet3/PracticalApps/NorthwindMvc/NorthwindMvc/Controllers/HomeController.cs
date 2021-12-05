a6ea8c063a4d13921a95c0a0c90a438972aa264c
zjtomoon <zjtomoon@outlook.com>
2021-12-05 13:50:07 +0800

    default commit

---
+++ Csharp8AndDotnet3/PracticalApps/NorthwindMvc/NorthwindMvc/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NorthwindMvc.Models;
+using Packt.Shared;
+
+namespace NorthwindMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private Northwind db;
+
+        public HomeController(ILogger<HomeController> logger,Northwind injectedContext)
+        {
+            _logger = logger;
+            db = injectedContext;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new HomeIndexViewModel
+            {
+                VisitorCount = (new Random()).Next(1, 1001),
+                Categories = db.Categories.ToList(),
+                Products = db.Products.ToList()
+            };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
