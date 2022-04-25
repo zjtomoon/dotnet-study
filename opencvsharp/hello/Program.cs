3a009c2bb823c8b56305a57f556563f0943eba7e
zjtomoon <zjtomoon@outlook.com>
2022-04-25 14:37:41 +0800

    hello opencvsharp

---
+++ opencvsharp/hello/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenCvSharp;
+
+namespace opencv4test
+{
+    class hello
+    {
+        static void Main(string[] args) {
+            Mat image = new Mat("D:\\images\\1.jpg", ImreadModes.Color);
+            Cv2.ImShow("demo", image);
+            Cv2.WaitKey(0);
+        }
+    }   
+}
