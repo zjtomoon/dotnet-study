bc0078a9e6187141e545de1563d36ebce64280db
zjtomoon <zjtomoon@outlook.com>
2022-09-20 20:57:07 +0800

    GenerateRandomPasswd

---
+++ myLibrary/GenerateRandomPasswd/GenerateRandomPasswd/GenerateRandomPasswd.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GenerateRandomPasswd
+{
+    public class GenerateRandomPasswd
+    {
+        public static string GenerateRandomCryptPasswd(int length)
+        {
+            byte[] rgb = new byte[length];
+            RNGCryptoServiceProvider rngCrypt = new RNGCryptoServiceProvider();
+            rngCrypt.GetBytes(rgb);
+            return Convert.ToBase64String(rgb);
+        }
+
+        public static string GetRandomPassword(int length)
+        {
+            const string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            StringBuilder sb = new StringBuilder();
+            Random rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = rnd.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ myLibrary/GenerateRandomPasswd/GenerateRandomPasswd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenerateRandomPasswd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 24;
+
+            string password1 = GenerateRandomPasswd.GenerateRandomCryptPasswd(length);
+            Console.WriteLine(password1);
+
+            string password2 = GenerateRandomPasswd.GetRandomPassword(length);
+            Console.WriteLine(password2);
+        }
+    }
+}
+
