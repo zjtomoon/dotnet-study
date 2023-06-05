7977365c4da15e5ba3c3679feb1c5c9df1068869
fwm <williamalex22@163.com>
2023-06-05 10:47:15 +0800

    csharp grpc demo

---
+++ grpc/grpc-demo01/client/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using client;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5143");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
